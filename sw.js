d4c1f26b3515ca8261ac331e965ee05c59e9e37c
Chijioke Udokporo <chijithemizzi@yahoo.co.uk>
2026-06-12 18:24:29 +0100

    fix: replace Wikipedia logo with user's loho.jpg

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ const CACHE_NAME = 'wc26-app-cache-v1';
 const ASSETS_TO_CACHE = [
   './index.html',
   './manifest.json',
-  './icon.svg'
+  './loho.jpg'
 ];
 
 self.addEventListener('install', (event) => {
